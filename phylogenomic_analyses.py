7552272e7421da7fd9deb148739fc31d960bca68
NaomiVCC05 <villalon@oregonstate.edu>
2026-05-07 10:43:23 -0700

    Added some code

--- phylogenomic_analyses.py
+++ phylogenomic_analyses.py
@@ -1,2 +1,31 @@
 #Create a list of unaligned files that we'll need to align
 
+#Import needed python modules 
+import os
+import sys
+import glob
+import subprocess
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio import Phylo
+
+
+#Cretate a string variable that stores the folder path to the input files
+file_name = "At_1G68100.fasta" 
+
+input_folder_path = "/shared/forsythe/BB485/Week06/Brass_CDS_seqs/"
+
+full_path_to_file = input_folder_path + file_name
+
+print(full_path_to_file)
+#Perform multiple sequence alignments 
+
+
+#Perform phylogenetic tree construction using IQtree 
+
+
+#Perform tree topology tests with biopython 
+
+
+
+#Store output
